896933ff5d6322ca4b8a9e1f70690f8a49bbd2e2
DaeV69 <70040510+DaeV69@users.noreply.github.com>
2022-03-01 19:28:18 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,134 +1,91 @@
-import express, { Express } from "express";
-import cors from "cors";
-import http from "http";
 import fs from "fs";
-import settings from "./settings";
-console.log(settings);
-import chalk from "chalk";
-import { v4 as uuidv4 } from "uuid";
-import {upload} from "./middlewares/upload"
+import https from "https";
+import stream from "stream";
+import querystring from "querystring";
 
-import type { CreateCollectionOptions } from "mongodb";
+import { ISaucenaoResult, ISaucenaoResultObj } from "types";
 
-/////////////////////////////////////////////// grounds for testing if dd connection can be established
-import mongoose from "mongoose";
-let serverState;
+import FormData from "form-data";
+import fileType from "file-type";
+import { stringify } from "querystring";
 
-mongoose.connection.on('error', function(error) {
-  console.log("error: ", error)
-})
-mongoose.connection.on('open', function (ref) {
-  console.log('Connected to Mongo server...')
-})
+/**
+ * creates a sauceNao instance
+ * @param api_key api_key you get from https://saucenao.com/user.php?page=search-api
+ */
+export class sauceNao {
+  public api_key: string | undefined;
 
-const dbConnection = mongoose.connect(settings.database_url);
+  /**
+   *
+   * @param input the path to teh faile you want the sauce (source) of
+   * @returns
+   */
+  public async getSauce(input: any): Promise<ISaucenaoResultObj> {
+    //might add options feild later
+    let form = new FormData();
+    let imgInputType: "url" | "file" | "readable stream" | "buffer";
+    form.append("output_type", 2);
+    if (this.api_key) form.append("api_key", this.api_key);
 
-///////////////////////////////////////////////////////////////////
-import TableOfContentsModel from "./models/TableOfContents";
+    switch (true) {
+      case typeof input === "string":
+        if (~input.search(/^https?:\/\//)) {
+          form.append("url", input);
+          imgInputType = "url";
+        } else {
+          form.append("file", fs.createReadStream(input));
+          imgInputType = "file";
+        }
+        break;
 
-//import AnimePic from "./models/AnimePic";
+      case input instanceof stream.Readable:
+        form.append("file", input);
+        imgInputType = "readable stream";
+        break;
 
-import CreateAnimePicModel from "./models/schemas/CreateAnimePicModel";
-import { ITableOfContents, AlbumSchemaType, INewPic } from "types";
+      case input instanceof Buffer:
+        let inputType = await fileType.fromBuffer(input);
+        form.append("file", input, {
+          filename: `file.${inputType?.ext}`,
+          contentType: inputType?.mime,
+        });
+        imgInputType = "buffer";
 
+        break;
 
-const typesOfModelsDictionary = {
-  "Anime Pic": CreateAnimePicModel,
-}
-
-const typesOfModels = ["Anime Pic"];
-
-if (!fs.existsSync("../files/thumbnail_files")) {
-  fs.mkdirSync("../files/thumbnail_files", { recursive: true })
-}
-
-class backendServer {
-  public express: Express;
-  public server: http.Server;
-  private backendURL = process.env.DEV ? "http://127.0.0.1:2234" : "http://localhost:2234" ;
-
-  constructor() {
-    this.express = express();
-    this.server = http.createServer(this.express);
-    console.log(this.backendURL)
-
-    this.registerExpressRoutes();
-
-    this.server.listen(settings.port, () =>
-      console.log(chalk.cyan(`[SERVER] Started on port ${settings.port.toString()}`))
-    );
-  }
-  private registerExpressRoutes(){
-    this.express.use(
-        cors({
-          origin: (origin: any, callback: any) => callback(null, true),
-        })
-      );
-      //oh fuck here we go 
-    this.express.use(express.json());
-    this.express.use(express.static("../files"));
-
-    this.express.post('/force-save', async (req, res) =>{
-        console.log(req);
-        return res.status(200).json({"sex":"sex"})
-
-    })
-    this.express.post('/check-status', async (req, res) =>{
-
-    return res.status(200).json({"sex":"sex"})
-
-  })
-
-  this.express.post('/add-picture', async (req, res) => {
-    const {url, album, type} = req.body;
-    const newEntry = new (typesOfModelsDictionary[type as AlbumSchemaType](album))({
-      id: uuidv4(),
-      file: url,
-      thumbnail_file: url,
-      album: album,
-      links: {discord: url},
-      has_results: false,
-    })
-    newEntry.save()
-  })
-
-  this.express.get('/album/:album', async (req, res) => {
-    const albumName = req.params.album; //im stop here cus how to get what model to use from this 
-    const album = ((await TableOfContentsModel.findOne({uuid: albumName})) as ITableOfContents);
-    const response = await (typesOfModelsDictionary[album.type as AlbumSchemaType](album.name)).find();
-    res.status(200).json(response);
-  })
-  this.express.post('/create-album', upload.single("thumbnail_file"), async (req, res) => {
-    const {type, name} = req.body;
-    
-    let thumbnail_file;
-    const newAlbum: ITableOfContents = {
-      name: name as string,
-      albumCoverImage: "thumbnail_files/image.svg",
-      type: type as AlbumSchemaType,
-      uuid: uuidv4()
-    };
-    if (req.file) {
-      thumbnail_file = req.file as Express.Multer.File;
-      newAlbum.albumCoverImage = thumbnail_file.filename;
+      default:
+        throw new TypeError("unrecognized input format");
     }
-  
-    const tableOfContentsEntry = new TableOfContentsModel(newAlbum);
-    tableOfContentsEntry.save();
+    let response = await new Promise<any>((resolve, reject) => {
+      form.submit("https://saucenao.com/search.php", (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
 
-    mongoose.connection.createCollection(newAlbum.name);
-    res.status(200).json("success");
-  })
-  this.express.get('/albums', async (req, res) => {      //table is a table of contents aka all of the albums or whatever databases
-    const albums = await TableOfContentsModel.find();
-    /* const wuh = await (await dbConnection).connection.db.listCollections().toArray()
-    console.log(wuh); */
-    return res.status(200).json(albums);
-  })
-  this.express.get('/types-of-models', async (req, res) => {
-    return res.status(200).json(typesOfModels);
-  })
+    try {
+      response.body = "";
+      response.setEncoding("utf8");
+      response.on("data", (chunk: any) => (response.body += chunk));
+      await new Promise((r) => response.on("end", r));
+
+      response.json = JSON.parse(response.body);
+      //console.log(response.json.results[0].data)
+    } catch (err) {
+      console.log(err); //err.response = response
+      throw err;
+    }
+
+    return {
+      resultArray: response.json.results,
+      inputType: imgInputType,
+    };
+  }
+
+  constructor(api_key: string) {
+    this.api_key = api_key;
+  }
 }
 
-}
-export default new backendServer();
+export default sauceNao;
